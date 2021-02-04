34bac53a6aa8e74aebb1dc12b7b8eb4c9fa6a37c
ptmertka <peter-mertka@uiowa.edu>
2021-02-04 17:37:06 -0600

    Fixed issue with rounding, eliminated break state ments and replaced with while loops, began JAVADOCS

--- oral_exam1/S4_ChangeComputation_Medium /src/Register.java
+++ oral_exam1/S4_ChangeComputation_Medium /src/Register.java
@@ -1,53 +1,84 @@
 import java.util.Random;
 import java.util.Scanner;
 
-
+/**
+ * Class Register:
+ * Holds the main fucntionality of the register object for the assignment,
+ * Contains the number of each type of bill and coin in the register and allows the user to make change based on user purchases
+ */
 public class Register {
 
     /**
-     * Things to change for later today, add java docs, add checkers with stringcasting inputs, checking if all positive
-     * integers, then casting to an int, convert everything from doubles to ints so that every money value is in cents
+     * the number of twenty dollar bills in the register
      */
     private int numTwenties;
+
+    /**
+     * the number of ten dollar bills in the register
+     */
     private int numTens;
+
+    /**
+     * the number of five dollar bills in the register
+     */
     private int numFives;
+
+    /**
+     * the number of ones dollar bills in the register
+     */
     private int numOnes;
+
+    /**
+     * the number of quarters in the register
+     */
     private int numQuarters;
+
+    /**
+     * the number of dimes in the register
+     */
     private int numDimes;
+
+    /**
+     * the number of nickels in the register
+     */
     private int numNickels;
+
+    /**
+     * the number of pennies in the register
+     */
     private int numPennies;
 
     Random random1 = new Random();  //creates a Random object in order generate a random number of bills and change in the drawer.
 
     public Register() {
-        numTwenties = random1.nextInt(16); //randomly generate value between 0-15, represents number of 20's in register
-        numTens = random1.nextInt(16); //randomly generate value between 0-15, represents number of 10's in register
-        numFives = random1.nextInt(16); //randomly generate value between 0-15, represents number of 5's in register
-        numOnes = random1.nextInt(16); //randomly generate value between 0-15, represents number of 1's in register
-        numQuarters = random1.nextInt(16); //randomly generate value between 0-15, represents number of quarters in register
-        numDimes = random1.nextInt(16); //randomly generate value between 0-15, represents number of dimes in register
-        numNickels = random1.nextInt(16); //randomly generate value between 0-15, represents number of nickels in register
-        numPennies = random1.nextInt(16); //randomly generate value between 0-15, represents number of pennies in register
+        numTwenties = 100;//random1.nextInt(16); //randomly generate value between 0-15, represents number of 20's in register
+        numTens = 100;//random1.nextInt(16); //randomly generate value between 0-15, represents number of 10's in register
+        numFives = 100;//random1.nextInt(16); //randomly generate value between 0-15, represents number of 5's in register
+        numOnes = 100;//random1.nextInt(16); //randomly generate value between 0-15, represents number of 1's in register
+        numQuarters =100 ;//random1.nextInt(16); //randomly generate value between 0-15, represents number of quarters in register
+        numDimes = 100;//random1.nextInt(16); //randomly generate value between 0-15, represents number of dimes in register
+        numNickels = 100;//random1.nextInt(16); //randomly generate value between 0-15, represents number of nickels in register
+        numPennies = 100;//random1.nextInt(16); //randomly generate value between 0-15, represents number of pennies in register
     }
 
-    double checkCashInRegister(){
-        double totalInRegister = 0.0;
-        totalInRegister = totalInRegister + (numTwenties * 20); //every line takes the number of each item in register and multiplies it by its value
+    int checkCashInRegister(){
+        int totalInRegister = 0;
+        totalInRegister = totalInRegister + (numTwenties * 2000); //every line takes the number of each item in register and multiplies it by its value
         //By doing this, it gets the cash value in the register, adding it to a running total
         System.out.println("Your have " + numTwenties + " twenty dollar bill(s)"); //This function also prints out the total amount of each bill and coin in the register
-        totalInRegister = totalInRegister + (numTens * 10);
+        totalInRegister = totalInRegister + (numTens * 1000);
         System.out.println("Your have " + numTens + " ten dollar bill(s)");
-        totalInRegister = totalInRegister + (numFives * 5);
+        totalInRegister = totalInRegister + (numFives * 500);
         System.out.println("Your have " + numFives + " five dollar bill(s)");
-        totalInRegister = totalInRegister + (numOnes);
+        totalInRegister = totalInRegister + (numOnes * 100);
         System.out.println("Your have " + numOnes + " one dollar bill(s)");
-        totalInRegister = totalInRegister + (numQuarters * .25);
+        totalInRegister = totalInRegister + (numQuarters * 25);
         System.out.println("Your have " + numQuarters + " quarter(s)");
-        totalInRegister = totalInRegister + (numDimes * .1);
+        totalInRegister = totalInRegister + (numDimes * 10);
         System.out.println("Your have " + numDimes + " dimes(s)");
-        totalInRegister = totalInRegister + (numNickels * .05);
+        totalInRegister = totalInRegister + (numNickels * 5);
         System.out.println("Your have " + numNickels + " nickel(s)");
-        totalInRegister = totalInRegister + (numPennies * .01);
+        totalInRegister = totalInRegister + (numPennies * 1);
         System.out.println("Your have " + numPennies + " pennies(s)");
         return totalInRegister; //returns the running total
 
@@ -65,7 +96,7 @@ public class Register {
      * @param nickels
      * @param pennies
      */
-    void returnChange(double amountOwed,int twenties, int tens, int fives, int ones, int quarters, int dimes, int nickels, int pennies){
+    void returnChange(int amountOwed,int twenties, int tens, int fives, int ones, int quarters, int dimes, int nickels, int pennies){
 
         boolean canCompute = true; //boolean temp variable that tells the loop if there is enough of the right money in the register to give exact change
         int twentiesUsed = 0; // all of these int variables are used to track how much of each denomination is used in producing change
@@ -78,96 +109,81 @@ public class Register {
         int penniesUsed = 0;
 
 
-
-            if (amountOwed >= 20) { //for each repeated chunk, it checks to see if the denomination of bill can be used to lower the amount owed
-
-                for (int i = 1; i <= numTwenties; i++) { //if it can, it checks to see how many of each bill to use, using a for loop
+        int i = 1;
+            if (amountOwed >= 2000) { //for each repeated chunk, it checks to see if the denomination of bill can be used to lower the amount owed
+                while(!((amountOwed - (i * 2000) >= 0) && (amountOwed - (i * 2000) < 2000)) && !(i == numTwenties)) {//if it can, it checks to see how many of each bill to use, using a while loop
                     //if any amount of a denomination is 0, the loop simply doesn't execute, if there is 1 or more bills, it goes through 1-number available, until a number gets
                     //you to a level where the amount remaining when that many bills is subtracted off is less than the bill value itself, or all of the bill/coin is used.
-
-                    if (((amountOwed - (i * 20) >= 0) && (amountOwed - (i * 20) < 20)) || (i == numTwenties)) {//once this condition is met, where a bill can be used for change
-                        twentiesUsed = i; //sets the number of bills being used to i
-                        amountOwed = amountOwed - (20 * i); //subtracts off the number of bills being used value to lower the total amount owed
-                        break; //once this value is found, breaks out of the loop to prevent unecessary iterations.
-                    }
+                    i++;
                 }
+                twentiesUsed = i; //sets the number of bills being used to i
+                amountOwed = amountOwed - (2000 * i); //subtracts off the number of bills being used value to lower the total amount owed
+
             } //this process is repeated for every value of change in the cash register
 
-            if (amountOwed >= 10) {
-                for (int i = 1; i <= numTens; i++) {
-                    if (((amountOwed - (i * 10) >= 0) && (amountOwed - (i * 10) < 10)) || (i == numTens)) {
-                        tensUsed = i;
-                        amountOwed = amountOwed - (10 * i);
-                        break;
-                    }
+            if (amountOwed >= 1000) {
+                i = 1;
+                while(!((amountOwed - (i * 1000) >= 0) && (amountOwed - (i * 1000) < 1000)) && !(i == numTens)) {
+                    i++;
                 }
+                tensUsed = i;
+                amountOwed = amountOwed - (1000 * i);
             }
+            if (amountOwed >= 500){
+                i = 1;
+                while(!((amountOwed - (i * 500) >= 0) && (amountOwed - (i * 500) < 500)) && !(i == numFives)) {
+                    i++;
+                }
+                fivesUsed = i;
+                amountOwed = amountOwed - (500 * i);
+            }
+            if (amountOwed >= 100){
+                i = 1;
+                while(!((amountOwed - (i * 100) >= 0) && (amountOwed - (i * 100) < 100)) && !(i == numOnes)) {
+                    i++;
+                }
+                onesUsed = i;
+                amountOwed = amountOwed - (100 * i);
+            }
+
+            if (amountOwed >= 25){
+
+                i = 1;
+                while(!((amountOwed - (i * 25) >= 0) && (amountOwed - (i * 25) < 25)) && !(i == numQuarters)) {
+                    i++;
+                }
+                quartersUsed = i;
+                amountOwed = amountOwed - (25 * i);
+            }
+
+            if (amountOwed >= 10){
+                i = 1;
+                while(!((amountOwed - (i * 10) >= 0) && (amountOwed - (i * 10) < 10)) && !(i == numDimes)) {
+                    i++;
+                }
+                dimesUsed = i;
+                amountOwed = amountOwed - (10 * i);
+            }
+
             if (amountOwed >= 5){
-                for (int i = 1; i <= numFives; i++) {
-                    if (((amountOwed - (i * 5) >= 0) && (amountOwed - (i * 5) < 5)) || (i == numFives)) {
-                        fivesUsed = i;
-                        amountOwed = amountOwed - (5*i);
-                        break;
-                    }
+                i = 1;
+                while(!((amountOwed - (i * 5) >= 0) && (amountOwed - (i * 5) < 5)) && !(i == numFives)) {
+                    i++;
                 }
-            }
-            if (amountOwed >= 1){
-                for (int i = 1; i <= numOnes; i++) {
-                    if (((amountOwed - (i * 1) >= 0) && (amountOwed - (i * 1) < 1)) || (i == numOnes)) {
-                        onesUsed = i;
-                        amountOwed = amountOwed - (1*i);
-                        break;
-
-                    }
-                }
-            }
-
-            if (amountOwed >= .25){
-
-                for (int i = 1; i <= numQuarters; i++) {
-
-                    if (((amountOwed - (i * .25) >= 0) && (amountOwed - (i * .25) < .25)) || (i == numQuarters)) {
-                        quartersUsed = i;
-                        amountOwed = amountOwed - (.25*i);
-                        break;
-
-                    }
-                }
-            }
-            if (amountOwed >= .10){
-                for (int i = 1; i <= numDimes; i++) {
-                    if (((amountOwed - (i * .1) >= 0) && (amountOwed - (i * .1) < .1)) || (i == numDimes)) {
-                        dimesUsed = i;
-                        amountOwed = amountOwed - (.1*i);
-                        break;
-
-                    }
-                }
-            }
-
-            if (amountOwed >= .05){
-                for (int i = 1; i <= numNickels; i++) {
-                    if (((amountOwed - (i * .05) >= 0) && (amountOwed - (i * .05) < 5)) || (i == numNickels)) {
-                        nickelsUsed = i;
-                        amountOwed = amountOwed - (.05*i);
-                        break;
-
-                    }
-                }
+                nickelsUsed = i;
+                amountOwed = amountOwed - (5 * i);
             }
             System.out.println("Amount Owed is " + amountOwed);
-            if (amountOwed >= .01){
-                for (int i = 1; i <= numPennies; i++) {
-                    if (((amountOwed - (i * .01) >= 0) && (amountOwed - (i * .01) < .01)) || (i == numPennies)) {
-                        penniesUsed = i;
-                        amountOwed = amountOwed - (.01*i);
-                        break;
-
-                    }
+            if (amountOwed >= 1){
+                i = 1;
+                while(!((amountOwed - (i * 1) >= 0) && (amountOwed - (i * 1) < 1)) && !(i == numTwenties)) {
+                    i++;
                 }
+                penniesUsed = i;
+                amountOwed = amountOwed - (1 * i);
             }
 
-            if( amountOwed > 0.01){ //if there is still an amount owed left greater than a penny, there is insufficient bills in the register to give exact change
+            if( amountOwed != 0){ //if there is still an amount owed left greater than a penny, there is insufficient bills in the register to give exact change
                 canCompute = false; //sets canCompute to false as a flag to the inability to calculate change
             }
 
@@ -222,25 +238,30 @@ public class Register {
         numPennies = numPennies - pennies;
     }
 
-    double sumBillsAndCoins(int twenties, int tens, int fives, int ones, int quarters, int dimes, int nickels, int pennies){
-        double total = 0;
-        total = total + (twenties * 20); //takes an custom input of bills and coins and sums them all together
-        total = total + (tens * 10);
-        total = total + (fives * 5);
-        total = total + (ones);
-        total = total + (quarters *.25);
-        total = total + (dimes * .1);
-        total = total + (nickels * .05);
-        total = total + (pennies * .01);
+    int sumBillsAndCoins(int twenties, int tens, int fives, int ones, int quarters, int dimes, int nickels, int pennies){
+        int total = 0;
+        total = total + (twenties * 2000); //takes an custom input of bills and coins and sums them all together
+        total = total + (tens * 1000);
+        total = total + (fives * 500);
+        total = total + (ones * 100);
+        total = total + (quarters *25);
+        total = total + (dimes * 10);
+        total = total + (nickels * 5);
+        total = total + (pennies * 1);
         return total;
+    }
+
+    int changeToCents(double value){
+        int valInCents = (int) (value * 100);
+        return  valInCents;
     }
 
     void handleSale(int twenties, int tens, int fives, int ones, int quarters, int dimes, int nickels, int pennies, double priceOfItem){
         addMoneyToRegister(twenties,tens,fives,ones,quarters, dimes, nickels, pennies); //adds the money being used to pay to the register, making it available to be used in change
-        double cashRecieved = sumBillsAndCoins(twenties,tens,fives,ones,quarters, dimes, nickels, pennies); //calculates the sum of money being used by the customer to pay
+        int newPriceOfItem = changeToCents(priceOfItem);
+        int cashRecieved = sumBillsAndCoins(twenties,tens,fives,ones,quarters, dimes, nickels, pennies); //calculates the sum of money being used by the customer to pay
 
-
-        returnChange(cashRecieved-priceOfItem, twenties,tens,fives,ones,quarters, dimes, nickels, pennies); //takes in the bills recieved and the amount of change owed and returns change if possible
+        returnChange(cashRecieved- newPriceOfItem, twenties,tens,fives,ones,quarters, dimes, nickels, pennies); //takes in the bills recieved and the amount of change owed and returns change if possible
 
 
 
