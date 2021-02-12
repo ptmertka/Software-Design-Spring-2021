4c73016d1d14f7e471ccae5a20a6699d52f53ad6
ptmertka <peter-mertka@uiowa.edu>
2021-02-11 23:42:32 -0600

    Began working on Javadocs

--- oral_exam1/S102_OneTimePad/src/KeyFile.java
+++ oral_exam1/S102_OneTimePad/src/KeyFile.java
@@ -13,57 +13,98 @@ import java.io.*;
 
 public class KeyFile {
 
+
+    /**
+     * integer: the length of the key as chosen by the user
+     */
     private int lengthKey;
 
+    /**
+     * integer array: an array that stores the key values themselves, randomaly generated numbers from 1-26
+     */
     private int[] key;
 
+    /**
+     * integer: an number between 1 and the length of the key, signifying where to start encoding from
+     */
     private int start;
 
+    /**
+     * String: the name of the keyFile, as chosen by the user
+     */
     private String filename;
 
-    Random random1 = new Random();
+    /**
+     * Random: random object that allows for the generation of random numbers
+     */
+    private Random random1 = new Random();
 
+
+    /**
+     * Class Constructor
+     * Creates new keyFiles, taking in the length of the key and what to name the file
+     * @param n integer: the length of the key to be created for this file
+     * @param whatToName String: the name of the file as chosen by the user, keyFile will become this name .txt
+     */
     public KeyFile(int n, String whatToName){
         lengthKey = n;
         filename = whatToName;
 
     }
 
+    /**
+     * Creates a randomly genarated key based upon the user's chosen length, key values are from 1-26 in value
+     */
     public void generateKey(){
-        key = new int[lengthKey];
-        start = random1.nextInt(lengthKey) + 1;
-        for(int i = 0; i< lengthKey; i++){
-            key[i] = random1.nextInt(26) + 1;
+
+        key = new int[lengthKey]; //initializes the key instance variable as an int array of length lengthKEy
+
+        start = random1.nextInt(lengthKey) + 1; //randomly chooses a start position between 1 and the max length of the key
+
+        for(int i = 0; i< lengthKey; i++){ //for every empty slot in the key
+
+            key[i] = random1.nextInt(26) + 1; //initialize that slot's value to a number between 1-26
         }
     }
-    private String arrayToString(){
-        String convertedString = "";
 
-        for(int i = 0; i < lengthKey - 1; i++){
-            convertedString = convertedString + Integer.toString(key[i]) + "," ;
+    /**
+     * Takes an integer array, aka the key array and converts it to a string so that it can be written to a file
+     * @return String: a string representation of the integer array that acts as the key
+     */
+    private String arrayToString(){
+
+        String convertedString = ""; //blank, generic string to hold converted value
+
+        for(int i = 0; i < lengthKey - 1; i++){ //for every value in the key array, except the last one
+
+            convertedString = convertedString + Integer.toString(key[i]) + "," ; //gets the value of the key, turns it into a character, and adds it to the return string, plus a comma
         }
 
-        convertedString = convertedString + Integer.toString(key[lengthKey-1]);
+        convertedString = convertedString + Integer.toString(key[lengthKey-1]); //adds the last number to the string, separately so that no comma is added
 
         return convertedString;
     }
 
+    /**
+     * Creates the actual txt file based on the instance variable of the object. Stores the file in a folder within the module itself
+     */
     public void createFile() {
-        try {
 
-            File file = new File("/nfs/s-l011/local/vol02/p/ptmertka/IdeaProjects/ptmertka_swd/oral_exam1/S102_OneTimePad/keyFiles/" + filename); //will contain.txt already
+        try { //try block to catch IOException errors
 
-            file.createNewFile();
+            File file = new File("/nfs/s-l011/local/vol02/p/ptmertka/IdeaProjects/ptmertka_swd/oral_exam1/S102_OneTimePad/keyFiles/" + filename); //creates a txt file in the proper keyFiles folder
 
-            FileWriter writer = new FileWriter(file);
+            file.createNewFile();//creates the file itself, throws an error if the file cannot be made
 
-            writer.write(Integer.toString(start) + "\n");
-            writer.write(arrayToString());
-            writer.flush();
-            writer.close();
+            FileWriter writer = new FileWriter(file); //allows the file to be written to
+
+            writer.write(Integer.toString(start) + "\n"); //appends the start value of the key as astring
+            writer.write(arrayToString()); //adds the key array as a string
+            writer.flush(); //clears the buffer but doesn't clear it
+            writer.close(); //closes the bufferstream
 
 
-        } catch (IOException ioe){
+        } catch (IOException ioe){ //if an exception happens, catches it, and tells the user
             System.out.println("Trouble reading and/or writing your file, please try again");
             System.out.println("Error Message: " + ioe.getMessage());
         }
--- oral_exam1/S102_OneTimePad/src/MessageFile.java
+++ oral_exam1/S102_OneTimePad/src/MessageFile.java
@@ -2,21 +2,46 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * Class MessageFile
+ * Creates a message file of an encoded message using a key. Holds relevant values like the number of message files created, the starting point of encryption, and the
+ * encoded message once it is converted
+ */
 public class MessageFile {
 
+    /**
+     * Integer: the value position of where in the key that encryption began
+     */
     private int start;
 
+    /**
+     * Static intger: represents how many message files are created. Since the user can create multiple message files, allows for them to have different names
+     */
     private static int fileNum = 1;
 
+    /**
+     * String: holds the text of the message after it has been encoded, message is chosen by the user
+     */
     private String encodedMessage;
 
+    /**
+     * Class Constructor
+     * Creates a new MessageFile Object, taking in a starting point of where encrpytion began, and the message after it has been encrypted
+     * @param n
+     * @param message
+     */
     public MessageFile(int n, String message){
         start = n;
         encodedMessage = message;
     }
+
+    /**
+     * Creates the message file using the objects parameters, saving it to a specific message folder in the module
+     */
     public void createFile(){
-        try {
-            //File dir = new File("/nfs/s-l011/local/vol02/p/ptmertka/IdeaProjectsptmertka_swd/oral_exam1/S102_OneTimePad/messages/");// not working to write a
+
+        try { //Try to catch a IOE exception
+
 
             File file = new File("/nfs/s-l011/local/vol02/p/ptmertka/IdeaProjects/ptmertka_swd/oral_exam1/S102_OneTimePad/messages/" + "message" + Integer.toString(fileNum)+".txt"); //will contain.txt already
 
@@ -40,7 +65,4 @@ public class MessageFile {
 
     }
 
-    public String getEncodedMessage() {
-        return encodedMessage;
-    }
 }
