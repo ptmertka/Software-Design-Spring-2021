24dc1224a2dbf2857c1ebe13e2d041453fa7aeaf
ptmertka <peter-mertka@uiowa.edu>
2021-02-16 00:26:08 -0600

    Finished mostly completing Sport Class, finished Football Class

--- oral_exam1/S5_Scoreboard_Hard/src/Football.java
+++ oral_exam1/S5_Scoreboard_Hard/src/Football.java
@@ -1,2 +1,130 @@
-public class Football {
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Football extends Sport{
+
+
+    /**
+     * Class constructor for Class Football, setting values based on user inputs
+     * @param home String: the name of the home team to be set
+     * @param away String: the name of the away team to be set
+     */
+    public Football(String home, String away){
+        setAwayTeam(away); //passes the strings back up to the parent classes instance variables
+        setHomeTeam(home);
+        String [] ScoringMethods = {"Touchdown", "Extra Point", "Two Point Conversion", "Safety", "Field goal"}; //specific string array sets the scoring methods
+        setScoringMethods(ScoringMethods);
+    }
+
+    @Override
+    public void advanceTimePeriod() {
+        setPeriod(getPeriod() + 1); //increments the period by one, in this case by quarters
+
+        if((getPeriod() > 4) && (getAwayScore() != getHomeScore())){ //if the game ends in regulation and there is not a tie
+            setGameOver(true); //end the game
+        }
+        else if(getPeriod() > 7){
+            setGameOver(true); //Only allows 3OT's per game
+        }
+
+
+    }
+
+    /**
+     * Gets a valid user input based on the number of options available for scoring in the football game
+     * @return int: the valid user input of their choice
+     */
+    @Override
+    public int getValidInput() {
+
+        int i = -1;
+        Scanner scanner = new Scanner(System.in); //scanner to handle inputs
+        while ((i < 1) || (i > 11)){ //while the user hasn't entered a number less than 1 or greater than 11
+            try{
+                i = scanner.nextInt(); //gets the user's input
+            }catch(InputMismatchException e){ //if the user enters an invalid input, return an error, get a new number
+                String badInput = scanner.next();
+                System.out.println("Please enter a valid, positive number");
+                continue; //move past bad input
+
+            }
+
+        }
+        return i;
+
+    }
+
+    /**
+     * Takes the user's choice of scoring method and applies its functionaliy based on the scoring action in football
+     * @param n Int:  the choice of the user of what to do with the game
+     */
+    @Override
+    public void applyScoreChoice(int n) {
+        if (n == 1){
+            setHomeScore(getHomeScore() + 6); //adds 6 points to the home team for a touchdown
+        }
+        else if (n == 2){
+            setHomeScore(getHomeScore() + 1); //adds 1 point to the home team for an extra point
+        }
+        else if (n==3 || n==4){
+            setHomeScore(getHomeScore() + 2); //adds 2 points to the home team for either a safety or two point conversion
+
+        }
+        else if (n == 5){
+            setHomeScore(getHomeScore() + 3); //adds 3 points to the home team for a field goal
+        }
+        else if(n ==6){
+            setAwayScore(getAwayScore() + 6); //adds 6 points to the away team for a touchdown
+        }
+        else if(n == 7){
+            setAwayScore((getAwayScore() + 1)); //adds 1 point to the away team for an extra point
+        }
+        else if (n == 8 || n == 9){
+            setAwayScore(getAwayScore() + 2); //adds 2 points to the away team for either a two point conversion or a safety
+        }
+        else if(n == 10){
+            setAwayScore(getAwayScore() + 3); //adds 3 points to the away team for a field goal
+        }
+        else{
+            advanceTimePeriod(); //increments the period of time, which will be option 11
+        }
+
+    }
+
+    /**
+     * Shows all of the scoring options and time options based on scoring methods and rules of football
+     */
+    @Override
+    public void displayOptions() {
+        String[] scoringMethods  = getScoringMethods(); // gets the array of scoring methods so that they can be printed off
+
+        for(int i = 0; i < scoringMethods.length; i++){
+            System.out.println(i + ". " + getHomeTeam() + " " + scoringMethods[i]);
+        }
+        System.out.println("11. Advance to next quarter");
+    }
+
+    /**
+     * Displays the time period of the game, with specific cathces for the game being over, and overtimes
+     */
+    @Override
+    public void displayPeriod() {
+
+        if(getPeriod() <= 4){ //handles normal quarters
+            System.out.println("Quarter " + getPeriod());
+        }
+        else if(getGameOver()){ //prints if game is over
+            System.out.println("Game Over");
+        }
+        else if(getPeriod() == 5){ //handles the next 3 overtimes
+            System.out.println("First Overtime");
+        }
+        else if(getPeriod() == 6){
+            System.out.println("Second Overtime");
+        }
+        else{
+            System.out.println("Third Overtime");
+        }
+
+    }
 }
--- oral_exam1/S5_Scoreboard_Hard/src/Sport.java
+++ oral_exam1/S5_Scoreboard_Hard/src/Sport.java
@@ -1,15 +1,17 @@
 /**
  * Class Sport
  * Holds the basic outline and generic form for all the sports classes. Holds getters and setters for basic instance variables
- * Implementation of method functionality will be present in subclasses
- * @autor Peter Mertka
+ * Implementation of method functionality will be present in subclasses, thus class Sport is an abstract class
+ * @author Peter Mertka
  * @version 2/15/2021
  */
 
 
 
 
-public class Sport {
+public abstract class Sport {
+
+
 
     /**
      * String: The name the home team
@@ -23,34 +25,185 @@ public class Sport {
     private String awayTeam;
 
     /**
-     * Integer: The score of the home team
+     * Integer: The score of the home team, always begins at zero
      */
 
-    private int homeScore;
+    private int homeScore = 0;
 
     /**
-     * Integer: The score of the away team
+     * Integer: The score of the away team, always begins at zero
      */
 
-    private int awayScore;
+    private int awayScore = 0;
 
     /**
-     * Integer: The time period of the game being played, wheter it is a quarter, a period, etc
+     * Integer: The time period of the game being played, wheter it is a quarter, a period, etc, always begins at one
      */
 
-    private int period;
+    private int period = 1;
 
     /**
      * String array[]: An array of strings representing the methods of scoring for each sport
      */
 
-    private String[] scoreingMethods;
+    private String[] scoringMethods;
 
     /**
      * Boolean: holds whether the game is over or not
      */
 
     private Boolean gameOver;
+
+    /**
+     * Getter for the name of the home team
+     * @return String: Name of the home team
+     */
+    public String getHomeTeam(){
+        return homeTeam;
+    }
+
+    /**
+     * Setter for the name of the home team
+     * @param name String: chosen by the user to name the home team
+     */
+    public void setHomeTeam(String name){
+        homeTeam = name;
+    }
+
+    /**
+     * Getter for the name of the away team
+     * @return String: name of the away team
+     */
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    /**
+     * Setter for the name of the away team
+     * @param name String: name of the away team as chosen by the user
+     */
+    public void setAwayTeam(String name) {
+        awayTeam = name;
+    }
+
+    /**
+     * Getter for the score of the home team
+     * @return int: the score of the home team
+     */
+    public int getHomeScore() {
+        return homeScore;
+    }
+
+    /**
+     * Setter for the homeScore, allows child class to augment score
+     * @param n int: what to change score to
+     */
+    public void setHomeScore(int n){
+        homeScore = n;
+    }
+
+    /**
+     * Setter for the awayScore, allows child class to augment score
+     * @param n int: what to change the score to
+     */
+    public void setAwayScore(int n){
+        awayScore = n;
+    }
+
+    /**
+     * Getter for the score of the away team
+     * @return int: score of the away team
+     */
+    public int getAwayScore() {
+        return awayScore;
+    }
+
+    /**
+     * Getter for the period of the game at the time
+     * @return int: The current time period of the game as it is occuring
+     */
+    public int getPeriod() {
+        return period;
+    }
+
+    /**
+     * Setter for the period of the game, allowing child classes to update the time of the game
+     * @param periodOfGame int: the next period to be set in the game
+     */
+    public void setPeriod(int periodOfGame) {
+        period = periodOfGame;
+    }
+
+    /**
+     * Getter for the gameOver Boolean
+     * @return Boolean: True or False as to whether the game is over or not
+     */
+    public Boolean getGameOver() {
+        return gameOver;
+    }
+
+    /**
+     * Setter for the gameOver boolean
+     * @param x boolean: allows for the game being over to be set
+     */
+    public void setGameOver(boolean x){
+        gameOver = x;
+
+    }
+
+    /**
+     * Getter for the list of scoring methods, each list will be sport specific
+     * @return A list of strings, storing the ways to score in the sport.
+     */
+    public String[] getScoringMethods() {
+        return scoringMethods;
+    }
+
+    /**
+     * Setter for the scoringMethods instance variable, allowing the child class to set the string of
+     * @param howToScore
+     */
+    public void setScoringMethods(String[] howToScore){
+        scoringMethods = howToScore;
+
+    }
+
+    /**
+     * Generic function used to display the score of the game in one neat line
+     */
+    public void displayScore(){
+
+        System.out.println("Score: " + homeTeam + ": " + Integer.toString(homeScore) + " - " + awayTeam + ": " + Integer.toString(awayScore));
+
+        //implement winner screen:
+    }
+
+    /**
+     * Abstract function that will advance the time period of the game, specific to each game's rules
+     */
+    public abstract void advanceTimePeriod();
+
+    /**
+     * Abastract function that will get a valid user choice for scoring options based on number of options available
+     * @return int: The chosen value of the user after input is validated
+     */
+    public abstract int getValidInput();
+
+    /**
+     * Abstract function that will take a user's choice and apply the proper functionality to the score/game
+     * @param n Int:  the choice of the user of what to do with the game
+     */
+    public abstract void applyScoreChoice(int n);
+
+    /**
+     * Abstract function that will display to the user their options for interacting with the game, made abstract due to differences betwen games
+     */
+    public abstract void displayOptions();
+
+    /**
+     * Abstract function that will display the period of the game, but within each class, will allow for specific changes based on rules of games
+     */
+    public abstract void displayPeriod();
 
 
 }
