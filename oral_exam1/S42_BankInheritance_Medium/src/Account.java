16a632ab58e96aec4fe3b6dac715b729b3902e2a
ptmertka <peter-mertka@uiowa.edu>
2021-02-23 22:39:52 -0600

    Getters and setters

--- oral_exam1/S42_BankInheritance_Medium/src/Account.java
+++ oral_exam1/S42_BankInheritance_Medium/src/Account.java
@@ -49,6 +49,88 @@ public class Account {
      */
     private static int numLoanAccountsCompany = 0;
 
+    /**
+     * Getter for the account UserName
+     * @return String: the name of the account owner
+     */
+    public String getAccountUserName() {
+        return accountUserName;
+    }
 
+    /**
+     * Setter for the accountUserName
+     * @param accountUserName String: The name of the user of the account
+     */
+    public void setAccountUserName(String accountUserName) {
+        this.accountUserName = accountUserName;
+    }
+
+    /**
+     * Getter for the
+     * @return
+     */
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public static int getNumSavingsAccounts() {
+        return numSavingsAccounts;
+    }
+
+    public static void setNumSavingsAccounts(int numSavingsAccounts) {
+        Account.numSavingsAccounts = numSavingsAccounts;
+    }
+
+    public static int getNumCheckingIndividual() {
+        return NumCheckingIndividual;
+    }
+
+    public static void setNumCheckingIndividual(int numCheckingIndividual) {
+        NumCheckingIndividual = numCheckingIndividual;
+    }
+
+    public static int getNumCheckingCompany() {
+        return numCheckingCompany;
+    }
+
+    public static void setNumCheckingCompany(int numCheckingCompany) {
+        Account.numCheckingCompany = numCheckingCompany;
+    }
+
+    public static int getNumLoanAccountsIndividual() {
+        return numLoanAccountsIndividual;
+    }
+
+    public static void setNumLoanAccountsIndividual(int numLoanAccountsIndividual) {
+        Account.numLoanAccountsIndividual = numLoanAccountsIndividual;
+    }
+
+    public static int getNumLoanAccountsCompany() {
+        return numLoanAccountsCompany;
+    }
+
+    public static void setNumLoanAccountsCompany(int numLoanAccountsCompany) {
+        Account.numLoanAccountsCompany = numLoanAccountsCompany;
+    }
 
 }
