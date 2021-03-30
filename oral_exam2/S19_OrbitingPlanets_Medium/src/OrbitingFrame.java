07577716c194a0e7460c963c97a217a014697029
ptmertka <peter-mertka@uiowa.edu>
2021-03-30 15:27:44 -0500

    Ideas have been had!

--- oral_exam2/S19_OrbitingPlanets_Medium/src/OrbitingFrame.java
+++ oral_exam2/S19_OrbitingPlanets_Medium/src/OrbitingFrame.java
@@ -1,15 +1,52 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
 public class OrbitingFrame extends JFrame {
 
     private final JPanel space;
+
+    private final Timer timer;
+
+    private ArrayList<Planet> planentList = new ArrayList<Planet>();
 
     public OrbitingFrame(){
         super("Orbiting Planets");
 
         space = new JPanel();
 
+
+    }
+    private class mouseListener implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent mouseEvent) {
+
+        }
+    }
+    private class SpacePanel extends JPanel{
 
     }
 
