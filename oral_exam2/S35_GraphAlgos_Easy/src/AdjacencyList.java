efa90b1a8561a1666b26a6c0f449f17319f195fd
ptmertka <peter-mertka@uiowa.edu>
2021-04-18 16:08:44 -0500

    Began Creating GraphAlgos class

---
+++ oral_exam2/S35_GraphAlgos_Easy/src/AdjacencyList.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ * Class AdjacencyList, holds all the logic and methods to create the AdjacencyList for the list of words
+ * Methods include how to see which words are one letter off, and methods to calculate the proper values necessary
+ * @author Peter Mertka
+ * @version 4.18.21
+ */
+public class AdjacencyList {
+
+    /**
+     * Hashmap of Strings and of LinkedLists of Strings: Stores the list of edges for each word
+     */
+    private HashMap<String, ArrayList<String>> adjl = new HashMap<String, ArrayList<String>>();
+
+
+
+
+}
+
